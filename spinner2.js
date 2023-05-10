e141f84d3ee8cf3f06076e7c882c79250dbd7605
Arian Ahmadi <ARIANAHMADI1@GMAIL.COM>
2023-05-10 06:23:08 -0400

    Update spinner2.js

--- spinner2.js
+++ spinner2.js
@@ -1,17 +1,9 @@
-process.stdout.write("hello from spinner2.js... \rheyyy\n");
-
-//inspirder by typerwriter.js
-
-const animation = ["\r|   ", "\r/   ", "\r-   ", "\r\\   "]
-let pause = 0; 
-
+process.stdout.write("hello from spinner2.js... \rheyyy \n");
+const animation = ["\r| ", "\r/ ", "\r- ","\r\\ ","\r| ","\n"];
+let pause = 0;
 for (const frame of animation) {
   setTimeout(() => {
     process.stdout.write(frame);
   }, pause);
   pause += 200;
 }
-
-setTimeout(() => {
-  process.stdout.write('\n');
-}, pause);
